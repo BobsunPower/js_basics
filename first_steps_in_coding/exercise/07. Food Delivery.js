8bec15639132694b25bce5193336d782574a8fd0
BobsunPower <bobsunpower@gmail.com>
2022-04-22 08:03:52 +0300

    Completed

--- first_steps_in_coding/exercise/07. Food Delivery.js
+++ first_steps_in_coding/exercise/07. Food Delivery.js
@@ -1,7 +1,7 @@
-function foodDelivery(lst){
-    let chikenMenu = 10.35 * Number(lst[0]);
+function foodDelivery(lst) {
+    let chickenMenu = 10.35 * Number(lst[0]);
     let fishMenu = 12.4 * Number(lst[1]);
     let vegMenu = 8.15 * Number(lst[2]);
-    let total = (chikenMenu + fishMenu + vegMenu) * 1.2 + 2.5;
+    let total = (chickenMenu + fishMenu + vegMenu) * 1.2 + 2.5;
     console.log(total);
 }
--- first_steps_in_coding/exercise/08. Basketball Equipment.js
+++ first_steps_in_coding/exercise/08. Basketball Equipment.js
@@ -1,1 +1,8 @@
-function(lst)
+function basketballEquipment(lst) {
+    subscription = Number(lst[0]);
+    sneakers = subscription * 0.6;
+    kit = sneakers * 0.8;
+    ball = kit * 0.25;
+    other = ball * 0.2;
+    console.log(subscription + sneakers + kit + ball + other);
+}
