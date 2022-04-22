30fdeeb0f3d85ef5fc68392d607d9d5aa0215f4d
BobsunPower <bobsunpower@gmail.com>
2022-04-22 07:41:43 +0300

    Completed

--- first_steps_in_coding/exercise/07. Food Delivery.js
+++ first_steps_in_coding/exercise/07. Food Delivery.js
@@ -1,1 +1,8 @@
-function foodDelivery()
+function foodDelivery(lst){
+    let chikenMenu = 10.35 * Number(lst[0])
+    let fishMenu = 12.4 * Number(lst[1])
+    let vegMenu = 8.15 * Number(lst[2])
+    let total = (chikenMenu + fishMenu + vegMenu) * 1.2 + 2.5
+    console.log(total)
+}
+foodDelivery(["2 ","4 ","3 "])
