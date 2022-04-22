cd890ff5fe7b8f235b4b29b7a1a6fddce23a499a
BobsunPower <bobsunpower@gmail.com>
2022-04-22 07:35:06 +0300

    Completed

--- first_steps_in_coding/exercise/06. Repainting.js
+++ first_steps_in_coding/exercise/06. Repainting.js
@@ -1,12 +1,12 @@
-function repaining(input) {
+function repaining(lst) {
     let nylonPrice = 1.5;
     let paintPrice = 14.5;
     let separatorPrice = 5;
     let bagsPrice = 0.4;
-    let nylonQuantity = Number(input[0]) + 2;
-    let paintQuantity = Number(input[1]) * 1.1;
-    let separatorQuantity = Number(input[2]);
-    let laborHours = Number(input[3]);
+    let nylonQuantity = Number(lst[0]) + 2;
+    let paintQuantity = Number(lst[1]) * 1.1;
+    let separatorQuantity = Number(lst[2]);
+    let laborHours = Number(lst[3]);
     let stockPrice = (nylonQuantity * nylonPrice) + (paintQuantity * paintPrice) + (separatorQuantity * separatorPrice) + bagsPrice;
     let laborPrice = stockPrice * 0.3;
     let total = laborHours * laborPrice + stockPrice;
