43e432bc92566f380d600758d591adfa55105f47
BobsunPower <bobsunpower@gmail.com>
2022-04-30 08:35:10 +0300

    Completed

---
+++ conditional_statements/exercise/02. Bonus Score.js
@@ -0,0 +1,17 @@
+function bonusScore(lst) {
+    let score = Number(lst[0]);
+    let bonus;
+    if (score <= 100) {
+        bonus = 5;
+    } else if (score <= 1000) {
+        bonus = score * 0.2;
+    } else {
+        bonus = score * 0.1;
+    }
+    if (score % 2 == 0) {
+        bonus += 1;
+    } else if (score % 10 == 5) {
+        bonus += 2;
+    }
+    console.log(`${bonus}\n${score + bonus}`)
+}
