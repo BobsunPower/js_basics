7800c409602cde41293cedd0bd3d8f690d67e6f4
BobsunPower <bobsunpower@gmail.com>
2022-05-03 20:09:02 +0300

    Completed

--- conditional_statements/exercise/03. Time + 15 Minutes.js
+++ conditional_statements/exercise/03. Time + 15 Minutes.js
@@ -3,10 +3,10 @@ function timePlus15Minutes(lst) {
     let minutes = Number(lst[1]) + 15;
     hours += parseInt(minutes / 60);
     minutes %= 60;
-    if (hours > 23){
+    if (hours > 23) {
         hours %= 24;
     }
-    if(minutes < 10 ){
+    if(minutes < 10 ) {
         minutes = "0" + minutes;
     }
     console.log(`${hours}:${minutes}`);
