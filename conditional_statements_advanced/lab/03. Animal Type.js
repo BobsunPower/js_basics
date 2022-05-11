f6c59b1b8b8a2f2e2eae608017e2294c84b8c686
BobsunPower <bobsunpower@gmail.com>
2022-05-11 20:04:46 +0300

    Completed

--- conditional_statements_advanced/lab/03. Animal Type.js
+++ conditional_statements_advanced/lab/03. Animal Type.js
@@ -1,10 +1,8 @@
 function animalType(lst) {
     let animal = lst[0];
     switch(animal) {
-        case "dog":
-            specie = "mammal"; break;
-        case "crocodile":case "tortoise": case "snake":
-            specie = "reptile"; break;
+        case "dog": specie = "mammal"; break;
+        case "crocodile":case "tortoise": case "snake": specie = "reptile"; break;
         default: specie = "unknown";
     }
     console.log(specie);
