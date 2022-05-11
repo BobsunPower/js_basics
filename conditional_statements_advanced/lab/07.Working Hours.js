bb73890d8bd8ae384261b99a7e81fce10d5603f4
BobsunPower <bobsunpower@gmail.com>
2022-05-11 19:17:23 +0300

    Completed

---
+++ conditional_statements_advanced/lab/07.Working Hours.js
@@ -0,0 +1,9 @@
+function workingHours(input) {
+    let time = Number(input[0]);
+    let dayOfWeek = input[1];
+    if (dayOfWeek != "Sunday" && time >= 10 && time <= 18) {
+        console.log("open");
+    } else {
+        console.log("closed");
+    }
+}
