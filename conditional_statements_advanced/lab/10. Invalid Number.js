f0beee64024d48604ba62e053e59f248a31ffa22
BobsunPower <bobsunpower@gmail.com>
2022-05-13 07:36:08 +0300

    Completed

--- conditional_statements_advanced/lab/10. Invalid Number.js
+++ conditional_statements_advanced/lab/10. Invalid Number.js
@@ -1,6 +1,6 @@
 function invalidNumber(lst) {
     let num = Number(lst[0]);
-    if ((num<100 || num>200) && num != 0) {
+    if ((num < 100 || num > 200) && num != 0) {
         console.log("invalid")
     }
 }
