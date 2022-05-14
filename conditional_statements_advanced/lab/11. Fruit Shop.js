bcc0391618d47248cb214fe7e6db0d3b844241bc
BobsunPower <bobsunpower@gmail.com>
2022-05-14 12:30:55 +0300

    Completed

--- conditional_statements_advanced/lab/11. Fruit Shop.js
+++ conditional_statements_advanced/lab/11. Fruit Shop.js
@@ -2,7 +2,7 @@ function fruitShop(lst) {
     let fruit = lst[0];
     let day = lst[1];
     let quantity = Number(lst[2]);
-    let price = -1;
+    let price;
     if ((day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")) {
         switch (fruit) {
             case "banana": price = 2.5; break; case "apple": price = 1.2; break; case "orange": price = 0.85; break; case "grapefruit": price = 1.45; break;
@@ -14,7 +14,7 @@ function fruitShop(lst) {
             case "kiwi": price = 3; break; case "pineapple": price = 5.6; break; case "grapes": price = 4.2; break;
         }
     }
-    if (price != -1) {
+    if (price) {
         console.log((price * quantity).toFixed(2));
     } else {
         console.log("error");
