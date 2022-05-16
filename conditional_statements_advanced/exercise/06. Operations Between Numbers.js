1ecc21816bf344ce1f4109fe170a9dd025ae8c77
BobsunPower <bobsunpower@gmail.com>
2022-05-16 11:23:24 +0300

    Completed

---
+++ conditional_statements_advanced/exercise/06. Operations Between Numbers.js
@@ -0,0 +1,18 @@
+function operationsBetweenNumbers(lst) {
+    let fst = Number(lst[0]);
+    let sec = Number(lst[1]);
+    let sign = lst[2];
+    let res;
+    let evenOrOdd = "odd";
+    switch(sign) {
+        case "+": res = fst + sec; break;
+        case "-": res = fst - sec; break;
+        case "*": res = fst * sec; break;
+        case "/": res = (fst / sec).toFixed(2); break;
+        case "%": res = fst % sec; break;
+    }
+    if (res % 2 == 0) {evenOrOdd = "even"};
+    if (!sec && (sign === "/" || sign === "%")) {console.log(`Cannot divide ${fst} by zero`);}
+    else if (sign == "/" || sign == "%") {console.log(`${fst} ${sign} ${sec} = ${res}`);}
+    else {console.log(`${fst} ${sign} ${sec} = ${res} - ${evenOrOdd}`);}
+}
