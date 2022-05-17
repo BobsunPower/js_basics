b96c3792ae9559551865a902fa7ede7917c1a3f0
BobsunPower <bobsunpower@gmail.com>
2022-05-17 17:53:44 +0300

    Completed

--- conditional_statements_advanced/exercise/08. On Time for the Exam.js
+++ conditional_statements_advanced/exercise/08. On Time for the Exam.js
@@ -6,12 +6,11 @@ function onTimeForTheExam(lst) {
     let examTime = examHours * 60 + examMinutes;
     let arrivalTime = arrivalHours * 60 + arrivalMinutes;
     let timeGap = Math.abs(examTime - arrivalTime);
-    if(examTime < arrivalTime) {
-        console.log("Late");
+    if (examTime < arrivalTime) {console.log("Late");
         if (timeGap >= 60 && (timeGap % 60) < 10) {console.log(`${Math.floor(timeGap / 60)}:${"0" + (timeGap % 60)} hours after the start`);}
         else if (timeGap >= 60) {console.log(`${Math.floor(timeGap / 60)}:${(timeGap % 60)} hours after the start`)}
         else {console.log(`${(timeGap % 60)} minutes after the start`);}}
-    else if(timeGap <= 30 || timeGap == 0) {console.log("On time"); if (timeGap > 0) console.log(`${timeGap} minutes before the start`)} 
+    else if (timeGap <= 30) {console.log("On time"); if (timeGap > 0) console.log(`${timeGap} minutes before the start`)} 
     else {console.log("Early");
         if (timeGap >= 60 && (timeGap % 60) < 10) {console.log(`${Math.floor(timeGap / 60)}:${"0" + (timeGap % 60)} hours before the start`)}
         else if (timeGap >= 60) {console.log(`${Math.floor(timeGap / 60)}:${(timeGap % 60)} hours before the start`)}
