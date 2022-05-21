1efd20dffd6d3a66d87664bd39e7efc8d1e6ed2f
BobsunPower <bobsunpower@gmail.com>
2022-05-21 18:36:01 +0300

    Completed

--- for_loop/lab/07. Sum Numbers.js
+++ for_loop/lab/07. Sum Numbers.js
@@ -1,6 +1,6 @@
 function sumOfNumbers(lst) {
-    let txt = lst[0];
+    let seq = lst[0];
     let vlu = 0;
-    for (let i = 0; i < txt.length; i++) {vlu += Number(txt[i])}
+    for (let i = 0; i < seq.length; i++) {vlu += Number(seq[i])}
     console.log(`The sum of the digits is:${vlu}`)
 }
