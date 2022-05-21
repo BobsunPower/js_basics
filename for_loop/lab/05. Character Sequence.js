61041ffbcc1ffc336630fbb73266a673473ef276
BobsunPower <bobsunpower@gmail.com>
2022-05-21 18:23:15 +0300

    Completed

--- for_loop/lab/05. Character Sequence.js
+++ for_loop/lab/05. Character Sequence.js
@@ -1,14 +1,4 @@
 function characterSequence(lst) {
-    let txt = lst[0]
-    let vlu = 0
-    for (let i = 0; i < txt.length; i++) {
-        switch(txt[i]) {
-            case "a": vlu += 1; break;
-            case "e": vlu += 2; break;
-            case "i": vlu += 3; break;
-            case "o": vlu += 4; break;
-            case "u": vlu += 5; break;
-        }
-}
-    console.log(vlu)
+    let txt = lst[0];
+    for (let i = 0; i < txt.length; i++) {console.log(txt[i])}
 }
--- for_loop/lab/06. Vowels Sum.js
+++ for_loop/lab/06. Vowels Sum.js
@@ -1,3 +1,14 @@
 function vowelsSum(lst) {
-    
+    let txt = lst[0]
+    let vlu = 0;
+    for (let i = 0; i < txt.length; i++) {
+        switch(txt[i]) {
+            case "a": vlu += 1; break;
+            case "e": vlu += 2; break;
+            case "i": vlu += 3; break;
+            case "o": vlu += 4; break;
+            case "u": vlu += 5; break;
+        }
+}
+    console.log(vlu)
 }
