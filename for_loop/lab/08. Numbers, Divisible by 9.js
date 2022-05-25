d45164cf5f537bdac7532c9262aafd2541fa94e0
BobsunPower <bobsunpower@gmail.com>
2022-05-26 00:07:47 +0300

    Completed

--- for_loop/lab/08. Numbers, Divisible by 9.js
+++ for_loop/lab/08. Numbers, Divisible by 9.js
@@ -1,10 +1,5 @@
 function numbersDivisibleBy9(lst) {
-    let fst = Number(lst[0]);
-    let sec = Number(lst[1]);
-    let vlu = 0;
-    let txt = "";
-    for (fst; fst <= sec; fst++) {if (fst % 9 == 0) {vlu += fst; txt += `${fst}\n`;}
-    }
-    console.log(`The sum: ${vlu}`);
-    console.log(txt);
+    let fst = Number(lst[0]); let sec = Number(lst[1]); let vlu = 0; let txt = "";
+    for (fst; fst <= sec; fst++) {if (fst % 9 == 0) {vlu += fst; txt += `${fst}\n`;}}
+    console.log(`The sum: ${vlu}`); console.log(txt);
 }
