13d0d04af343728aa02be751e084e644135275bc
BobsunPower <bobsunpower@gmail.com>
2022-05-27 08:03:30 +0300

    Completed

--- for_loop/exercise/05. Salary.js
+++ for_loop/exercise/05. Salary.js
@@ -1,14 +1,13 @@
 function salary(lst) {
     let facebook = 150; let instagram = 100; let reddit = 50;
-    let oppenedTabs = Number(lst[0]);
+    let openedTabs = Number(lst[0]);
     let salary = Number(lst[1]);
     let index = 2;
-    for (let i = 0; i < oppenedTabs; i++) {
-        let tab = lst[index++];
-        switch(tab){
+    for (let i = 0; i < openedTabs; i++) {let tab = lst[index++];
+        switch(tab) {
             case "Facebook": salary -= facebook; break;
             case "Instagram": salary -= instagram; break;
             case "Reddit": salary -= reddit; break;}
-        if(salary <= 0){console.log("You have lost your salary."); break;}}
+        if (salary <= 0) {console.log("You have lost your salary."); break;}}
     if (salary > 0) {console.log(salary);}
 }
