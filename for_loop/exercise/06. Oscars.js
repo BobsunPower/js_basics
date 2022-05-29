ca2e1f6d0e96b9735565f217f545d189de447b2b
BobsunPower <bobsunpower@gmail.com>
2022-05-29 12:11:59 +0300

    Completed

--- for_loop/exercise/06. Oscars.js
+++ for_loop/exercise/06. Oscars.js
@@ -5,8 +5,8 @@ function oscars(lst) {
     let judges = Number(lst[idx++]);
     let nominated = false;
     for (let i = 0; i < judges; i++)
-    {let name = lst[idx++]; let cntPts = Number(lst[idx++]); pts += (name.length * cntPts) / 2;
-        if (pts > 1250.5) {nominated = true; break;}    }
+    {pts += (lst[idx++].length * Number(lst[idx++])) / 2;
+        if (pts > 1250.5) {nominated = true; break;}}
     if (nominated) {console.log(`Congratulations, ${actor} got a nominee for leading role with ${pts.toFixed(1)}!`);}
     else {console.log(`Sorry, ${actor} you need ${(1250.5 - pts).toFixed(1)} more!`);}
 }
