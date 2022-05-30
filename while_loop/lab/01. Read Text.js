e893454aca0d5eddf5afb9a3147eab449f7997de
BobsunPower <bobsunpower@gmail.com>
2022-05-30 07:35:10 +0300

    Completed

--- while_loop/lab/01. Read Text.js
+++ while_loop/lab/01. Read Text.js
@@ -1,4 +1,4 @@
 function readText(lst) {
     let idx = 0; let txt = lst[idx++];
-    while (txt != "Stop") {console.log(txt);txt = lst[idx++];}
+    while (txt != "Stop") {console.log(txt); txt = lst[idx++];}
 }
